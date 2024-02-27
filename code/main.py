31ee7a7aaf25b6dcec41b7be49a322c8c923b952
ktde24 <kjjgbjbe24@gmail.com>
2024-02-27 11:38:05 +0900

    host 수정

--- code/main.py
+++ code/main.py
@@ -108,8 +108,8 @@ def format_minutes(minutes):
 # Mysql 연결
 def connect_to_database():
     return pymysql.connect(
-        host='172.31.143.66',
-        port = 3306,
+        host='172.23.244.171',
+        #port = 3306,
         user='guest',
         password='0000',
         database='ChatbotTest',
